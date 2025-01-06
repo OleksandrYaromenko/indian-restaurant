99fb184eeda1ccd8d29ea97a06f727e0a6dbb446
OleksandrYaromenko <113725082+OleksandrYaromenko@users.noreply.github.com>
2025-01-06 11:21:01 +0100

    eslinc

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,44 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import prettier from 'eslint-plugin-prettier'
+import tsParser from '@typescript-eslint/parser'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist'], // Ігноруємо папку dist
+  },
+  {
+    files: ['**/*.{ts,tsx}'], // Цільові файли
     languageOptions: {
+      parser: tsParser, // Використовуємо TypeScript парсер
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      globals: globals.browser, // Додати глобальні змінні
     },
     plugins: {
+      react: react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: prettier,
+      '@typescript-eslint': tseslint,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }], // Підсвічує консольні логи, але не блокує їх
+      'no-trailing-spaces': 'error', // Видаляє зайві пробіли в кінці рядка
+      'no-multi-spaces': 'error', // Забороняє кілька пробілів між словами
+      'no-whitespace-before-property': 'error', // Забороняє пробіли перед властивостями об'єктів
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'prettier/prettier': 'error', // Форматує код, включаючи видалення зайвих пробілів
     },
   },
-)
+]
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-
-
 import './App.css'
+import Header from './components/Header/header'
 function App() {
- 
-
   return (
     <>
-     
+      <Header />
     </>
   )
 }
