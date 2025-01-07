6ef99a91df1794a0965c62ae14943e8a01c6566a
OleksandrYaromenko <113725082+OleksandrYaromenko@users.noreply.github.com>
2025-01-07 11:01:33 +0100

    eslinc

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,32 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from '@typescript-eslint/eslint-plugin'
-import prettier from 'eslint-plugin-prettier'
-import tsParser from '@typescript-eslint/parser'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
-    ignores: ['dist'], // Ігноруємо папку dist
-  },
-  {
-    files: ['**/*.{ts,tsx}'], // Цільові файли
-    languageOptions: {
-      parser: tsParser, // Використовуємо TypeScript парсер
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      globals: globals.browser, // Додати глобальні змінні
-    },
-    plugins: {
-      react: react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      prettier: prettier,
-      '@typescript-eslint': tseslint,
-    },
     rules: {
-      'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
-      'no-console': ['warn', { allow: ['warn', 'error'] }], // Підсвічує консольні логи, але не блокує їх
-      'no-trailing-spaces': 'error', // Видаляє зайві пробіли в кінці рядка
-      'no-multi-spaces': 'error', // Забороняє кілька пробілів між словами
-      'no-whitespace-before-property': 'error', // Забороняє пробіли перед властивостями об'єктів
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'no-console': 'warn',
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector:
+            "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+          message: 'Unexpected property on console object was called'
+        }
       ],
-      '@typescript-eslint/no-unused-vars': 'error',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      'prettier/prettier': 'error', // Форматує код, включаючи видалення зайвих пробілів
-    },
-  },
-]
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'no-trailing-spaces': ['error'],
+      'semi-spacing': ['error', { before: false, after: true }],
+      'no-multi-spaces': ['error'],
+      'space-in-parens': ['error', 'never']
+    }
+  }
+];
--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,9 +1,9 @@
 export default function Header(): JSX.Element {
-  console.log('sdfhdfghdf')
+  console.log('jghdfhjghdsfjkh');
 
   return (
     <header>
       <h1> Hello World</h1>
     </header>
-  )
+  );
 }
